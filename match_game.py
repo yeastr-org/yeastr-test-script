29c325ff61ba81d294df1661f45bdbc123bf602f
castix <castix@autistici.org>
2025-08-17 18:26:01 +0200

    ci: test match game everywhere

---
+++ match_game.py
@@ -0,0 +1,190 @@
+from yeastr.bootstrapped import *
+from yeastr.backport_match import backport_match
+from abc import ABC, abstractmethod
+
+@backport_match()
+def test_mapping():
+    actions = [
+        {"text": "ok", "color": "red"},
+        {"text": "<b>ok</b>", "color": "red", "style": "bold"},
+        {"sleep": "1s"},
+        {"sound": "file:///path.oga", "format": "ogg"},
+        {"sound": "file:///path.mp9", "format": "mp9"},
+    ]
+    for action in actions:
+        match action:
+            case {"text": message, "color": c}:
+                print(f'ui.set_text_color({c})')
+                print(f'ui.display({message})')
+            case {"sleep": duration}:
+                print(f'ui.wait({duration})')
+            case {"sound": url, "format": "ogg"}:
+                print(f'ui.play({url})')
+            case {"sound": _, "format": _}:
+                print('warning("Unsupported audio format")')
+test_mapping()
+
+class Room(ABC):
+    name = 'Room'
+    #objects = []
+    #directions = {}
+
+    @with_functional()
+    def describe(self):
+        print('Room:', self.name)
+        print('Objects:', ', '.join(self.objects))
+        print('Directions:', ', '.join(emap(
+            lambda direction, room:
+                f'{direction}={room.name}',
+            self.directions.items()
+        )))
+
+    @abstractmethod
+    def neighbor(self, direction): ...
+
+    @property
+    def exits(self):
+        return self.directions.keys()
+
+class MainRoom(Room):
+    name = 'Main'
+
+    def neighbor(self, direction):
+        return self.directions.get(direction, self)
+
+class KitchenRoom(Room):
+    name = 'Kitchen'
+
+    def neighbor(self, direction):
+        return self.directions.get(direction, self)
+
+class MirrorsRoom(Room):
+    name = 'Mirrors'
+
+    def neighbor(self, direction):
+        if direction == 'back':
+            return MainRoom()
+        print('You get lost and starved to death')
+        raise Break('mainloop')
+
+MainRoom.directions = {
+    'est': KitchenRoom(),
+    'north': MirrorsRoom(),
+}
+KitchenRoom.directions = {
+    'west': MainRoom(),
+}
+MirrorsRoom.directions = {
+    'back': MainRoom(),
+}
+
+def reset_rooms():
+    MainRoom.objects = ['PC', 'Bed']
+    MirrorsRoom.objects = ['Mirror', ]
+    KitchenRoomobjects = ['Cofee', 'Moka']
+
+
+class Character:
+    def __init__(self):
+        self.objects = []
+
+    def get(self, obj, current_room):
+        if obj in current_room.objects:
+            if obj == 'Mirror':
+                print('You became a narcisist, forgot to eat then died')
+                raise Break('mainloop')
+            current_room.objects.remove(obj)
+            self.objects.append(obj)
+            return obj
+        print('Endless research lead to death')
+        raise Break('mainloop')
+
+    def drop(self, obj, current_room):
+        if obj in self.objects:
+            self.objects.remove(obj)
+            current_room.objects.append(obj)
+        else:
+            print('You looked hard, then started to look inside your body, you\'re now dead in a pool of blood')
+            raise Break('mainloop')
+
+class Event:  # such a mock, multithreading events...
+    def __init__(self):
+        self.data = False
+    def get(self):
+        return self.data.pop() if self.data else False
+    def set(self, evt):
+        self.data = [evt]
+
+class Click:
+    def __init__(self, position):
+        self.position = position
+
+class KeyPress:
+    def __init__(self, kn):
+        self.key_name = kn
+
+
+@backport_match(custom_globals=globals(), debug=True)
+@with_foreach()
+def play():
+    character = Character()
+    reset_rooms()
+    current_room = MainRoom()
+    event = Event()
+    with While(True) as mainloop:
+        print(f'In your pockets: {character.objects}')
+        command = input('Command:')
+        match command.split():
+            case ["quit"]:
+                print("Goodbye!")
+                mainloop.Break
+            case ["look"]:
+                current_room.describe()
+            case ["get", obj]:
+                character.get(obj, current_room)
+            case ["north"] | ["go", "north"]:
+                current_room = current_room.neighbor("north")
+            case ["go", ("north" | "south" | "east" | "west") as direction]:
+                print(f'You tried to go {direction}')
+                print('but nah...')
+            case ["go", direction] if direction in current_room.exits:
+                current_room = current_room.neighbor(direction)
+            case ["go", _]:
+                print("Sorry, you can't go that way")
+            case ["get", obj] | ["pick", "up", obj] | ["pick", obj, "up"]:
+                character.get(obj, current_room)
+            case ["drop", *objects, 'Hearth']:
+                print('You don\'t have one, died')
+                mainloop.Break
+            case ["drop", *objects]:
+                print(f'dropping {objects}')
+                for obj in objects:
+                    character.drop(obj, current_room)
+            case ["click", x, y] if 'PC' in character.objects:
+                print('emulating click', x, y)
+                event.set(Click(position=(x, y)))
+            case ["press", key_name] if 'PC' in character.objects:
+                print('emulating keypress', key_name)
+                event.set(KeyPress(key_name))
+            case _:
+                print(f"Sorry, I couldn't understand {command!r}")
+
+        match event.get():
+            case Click(position=(x, y)):
+                print(f'You definitly clicked {x=} {y=}')
+            case KeyPress(key_name="Q") | KeyPress(key_name="q"):
+                mainloop.Break
+            case KeyPress(key_name="up_arrow"):
+                current_room = current_room.neighbor('north')
+            case KeyPress(key_name="s") if 'Bed' in character.objects:
+                print('zzz')
+            case KeyPress():
+                print('keystroke ignored')
+                pass # Ignore other keystrokes
+            case False:
+                print('no event')
+            case other_event:
+                raise ValueError(f"Unrecognized event: {other_event}")
+
+if __name__ == '__main__':
+    play()
---
+++ test_match_game.py
@@ -0,0 +1,37 @@
+import match_game
+
+match_game.test_mapping()
+
+def inputs():
+    yield 'pick PC up'
+    yield 'press Q'
+
+    yield 'go east'
+    yield 'look'
+    yield 'get Bed'
+    yield 'look'
+    yield 'get PC'
+    yield 'look'
+    yield 'press s'
+    yield 'drop PC Bed'
+    yield 'press s'
+    yield 'north'
+    yield 'pick up Mirror'
+
+    yield 'get PC'
+    yield 'click 50 100'
+    yield 'press s'
+    yield 'pick up Bed'
+    yield 'press s'
+    yield 'quit'
+
+inputs = inputs()
+match_game.input = lambda _: next(inputs)
+
+exec(compile(match_game.play.__source__, 'match_game_play.py', 'exec'), match_game.__dict__)
+match_game.play()
+print('GAME OVER')
+match_game.play()
+print('GAME OVER')
+match_game.play()
+print('GAME OVER')
